9bbdf8be4c8ae32385da3b6f0b42712f205ce266
makkabosu <lambddri@gmail.com>
2014-03-26 22:07:46 +0900

    とりあえず完成

--- index.php
+++ index.php
@@ -1,20 +1,57 @@
 <?php 
-
+date_default_timezone_set("Asia/Tokyo");
 $dataFile = 'bbs.dat';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+session_start();
 
-	$message =  $_POST['message'];
-	$user = $_POST['user'];
-
-	$newData = $message . "\t" . $user . "\n";
-
-	$fp = fopen($dateFile, 'a');
-	fwrite($fp, $newData);
-	fclose($fp);
+function setToken(){
+	$token = sha1(uniqid(mt_rand(), true));
+	$_SESSION['token'] = $token;
 }
+
+function checkToken(){
+	if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token'])) {
+		echo "不正なPOSTが行われました！";
+		exit;
+	}
+}
+
+function h($s){
+	return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' &&
+	isset($_POST['message']) &&
+	isset($_POST['user'])) {
+
+	checkToken();
+
+	$message =  trim($_POST['message']);
+	$user = trim($_POST['user']);
+
+	if ($message !== ''){
+
+		$user = ($user === '') ? '名無しさん' : $user;
+		
+		$message = str_replace("\t", ' ', $message);
+		$user = str_replace("\t", ' ', $user);
+
+		$postedAt = date('Y-m-d H:i:s');
+
+		$newData = $message . "\t" . $user .  "\t" . $postedAt . "\n";
+
+		$fp = fopen($dataFile, 'a');
+		fwrite($fp, $newData);
+		fclose($fp);
+	}
+} else {
+	setToken();
+}
+$posts = file($dataFile, FILE_IGNORE_NEW_LINES);
+
+$posts = array_reverse($posts);
  ?>
- <!doctype html>
+<!doctype html>
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
@@ -26,10 +63,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		message: <input type="text" name="message">
 		user: <input type="text" name="user">
 		<input type="submit" value="投稿">
+		<input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
 	</form>
-	<h2>投稿件数 (0件) </h2>
+	<h2>投稿件数 (<?php echo count($posts); ?>件) </h2>
 	<ul>
-		<li>まだ投稿はありません。</li>
+		<?php if (count($posts)) : ?>
+			<?php foreach ($posts as $post) : ?>
+			<?php list($message, $user, $postedAt) = explode("\t", $post); ?>
+				<li><?php echo h($message); ?> (<?php echo h($user); ?>) - <?php echo h($postedAt); ?></li>
+			<?php endforeach; ?>
+		<?php else : ?>
+			<li>まだ投稿はありません。</li>
+		<?php endif ; ?>
 	</ul>
 </body>
 </html>
